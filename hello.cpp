e62e52f74ca1d6bf2c8e14d9693b1f845ddaae1f
Tejas <tejasnaikj9@gmail.com>
2025-03-16 21:40:37 +0530

    Testing webhook trigger

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     cout << "Hello, Jenkins!" << endl;
-    cout << "Is webhook started? -3" << endl;
+    cout << "Is webhook started? -4" << endl;
     return 0;
 }

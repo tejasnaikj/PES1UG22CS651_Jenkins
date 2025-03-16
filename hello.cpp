0bf2f62fcb60bc13dc126464a897821b266495d4
Tejas <tejasnaikj9@gmail.com>
2025-03-16 21:31:34 +0530

    Triggered-2

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     cout << "Hello, Jenkins!" << endl;
-    cout << "Is webhook started?" << endl;
+    cout << "Is webhook started? -2" << endl;
     return 0;
 }
